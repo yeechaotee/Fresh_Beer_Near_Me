4c4117e9116e25cd67496d4359c3a8c8480b3524
yeechaotee <yechaotee1995@gmail.com>
2023-07-06 21:05:56 +0800

    initial commit

---
+++ navigation/screens/DiscoverScreen.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function DiscoverScreen({ navigation }) {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Discover Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textStyle: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "white",
+  },
+});

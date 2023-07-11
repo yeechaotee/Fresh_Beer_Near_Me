2afc4b2f2ab9d65b5f650309924ddddfa1ec5a8b
janmichaelortega <janmichaelortega1994@gmail.com>
2023-07-11 17:02:26 +0800

    map updates

---
+++ navigation/screens/InputAutoComplete.js
@@ -0,0 +1,42 @@
+import { GOOGLE_API_KEY } from "../../environments";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { Text, StyleSheet } from "react-native";
+
+export default function InputAutoComplete({
+  label,
+  placeholder,
+  onPlaceSelected,
+}) {
+  return (
+    <>
+      <Text style={styles.textStyle}>{label}</Text>
+      <GooglePlacesAutocomplete
+        onPlaceSelected={() => {}}
+        styles={{ textInput: styles.input }}
+        placeholder={placeholder || ""}
+        fetchDetails
+        onPress={(data, details = null) => {
+          onPlaceSelected(details);
+          console.log(data, details);
+        }}
+        query={{
+          key: GOOGLE_API_KEY,
+          language: "en",
+        }}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontWeight: "bold",
+    color: "#292929",
+  },
+
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+});
--- navigation/screens/MapsScreen.js
+++ navigation/screens/MapsScreen.js
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Pressable,
+  StyleSheet,
+  View,
+  Dimensions,
+  Button,
+  Image,
+  Text,
+} from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import darkModeStyle from "./darkMode.json";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { GOOGLE_API_KEY } from "../../environments";
+import Constants from "expo-constants";
+import InputAutoComplete from "./InputAutoComplete";
+import { endAsyncEvent } from "react-native/Libraries/Performance/Systrace";
+import GetRideModal from "./GetRide";
 
 const { width, height } = Dimensions.get("window");
-
-mapStyle = [{}];
 
 // location markers
 // let locationsOfInterest = [
@@ -32,32 +40,72 @@ mapStyle = [{}];
 
 export default function MapsScreen() {
   // states
-  const [location, setLocation] = useState({
+  const chekInPress = () => {
+    console.log("User Location:", userLocation);
+  };
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalVisible(true);
+  };
+
+  const [userLocation, setUserLocation] = useState({
     latitude: 1.3525271913212642,
     latitudeDelta: 0.38941821562666146,
     longitude: 103.85964151471853,
     longitudeDelta: 0.1941436529159546,
   });
 
-  const [userLocation, setUserLocation] = useState(null);
+  const [destination, setDestination] = useState(null);
 
-  const onRegionChange = (region) => {
-    console.log(region);
-    setUserLocation(region);
+  const mapRef = useRef(null);
+
+  const moveTo = async (position) => {
+    const camera = await mapRef.current?.getCamera();
+    if (camera) {
+      camera.center = position;
+      mapRef.current?.animateCamera(camera, { duration: 1000 });
+    }
   };
 
-  const showLocationsOfInterest = () => {
-    return locationsOfInterest.map((item, index) => {
-      return (
-        <Marker
-          key={index}
-          coordinate={item.location}
-          title={item.title}
-          description={item.description}
-        />
-      );
-    });
+  const onPlaceSelected = (details, flag) => {
+    console.log("Selected Place:", details);
+    console.log("Flag:", flag);
+    if (details && details.geometry && details.geometry.location) {
+      const position = {
+        latitude: details.geometry.location.lat,
+        longitude: details.geometry.location.lng,
+      };
+
+      // Perform different actions based on the flag value
+      if (flag === "userLocation") {
+        // Handle user location
+        setUserLocation(position);
+      } else if (flag === "destination") {
+        // Handle destination location
+        setDestination(position);
+      } else {
+        // Invalid flag value
+      }
+      moveTo(position);
+    } else {
+      console.log("No place selected");
+    }
   };
+
+  // const showLocationsOfInterest = () => {
+  //   return locationsOfInterest.map((item, index) => {
+  //     return (
+  //       <Marker
+  //         key={index}
+  //         coordinate={item.location}
+  //         title={item.title}
+  //         description={item.description}
+  //       />
+  //     );
+  //   });
+  // };
 
   useEffect(() => {
     console.log("use effect run");
@@ -73,7 +121,7 @@ export default function MapsScreen() {
       try {
         console.log("trying get location");
         let coords = await Location.getCurrentPositionAsync();
-        setLocation(coords);
+        setUserLocation(coords);
         console.log("location");
         console.log(coords);
         console.log("coordinates recorded");
@@ -87,36 +135,83 @@ export default function MapsScreen() {
   return (
     <View style={styles.viewStyle}>
       <MapView
+        ref={mapRef}
         style={styles.map}
         provider={PROVIDER_GOOGLE}
-        initialRegion={location}
-        onRegionChange={onRegionChange}
+        initialRegion={userLocation}
         customMapStyle={darkModeStyle}
       >
         {userLocation && (
           <Marker
+            draggable
             coordinate={userLocation}
             title="Your Location"
             description="You are here"
+            onDragEnd={(e) => setUserLocation(e.nativeEvent.coordinate)}
+          />
+        )}
+        {destination && (
+          <Marker
+            draggable
+            coordinate={destination}
+            title="destination"
+            description="Your destination"
+            onDragEnd={(e) => setDestination(e.nativeEvent.coordinate)}
           />
         )}
         {
           //showLocationsOfInterest()
         }
       </MapView>
-      <View style={styles.userLocation}>
-        <GooglePlacesAutocomplete
-          styles={{ textInput: styles.input }}
-          placeholder="Search"
-          onPress={(data, details = null) => {
-            // 'details' is provided when fetchDetails = true
-            console.log(data, details);
-          }}
-          query={{
-            key: GOOGLE_API_KEY,
-            language: "en",
+      <View style={styles.userLocationContainer}>
+        <InputAutoComplete
+          label="My Location"
+          onPlaceSelected={(details) => {
+            onPlaceSelected(details, "userLocation");
           }}
         />
+        <InputAutoComplete
+          label="My Destination"
+          onPlaceSelected={(details) => {
+            onPlaceSelected(details, "destination");
+          }}
+        />
+        <View
+          style={{
+            width: "50%",
+            alignSelf: "center",
+          }}
+        >
+          <Button title="Take me There" onPress={handleOpenModal} />
+        </View>
+      </View>
+      <View>
+        <GetRideModal
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+        />
+      </View>
+      <View style={styles.checkIn}>
+        <Pressable
+          onPress={chekInPress}
+          style={{
+            flexDirection: "row",
+            backgroundColor: "#ffa31a",
+            borderRadius: 30,
+            alignContent: "center",
+            alignItems: "center",
+            width: 10,
+          }}
+        >
+          <Image
+            source={require("../../assets/navIcons/goal.png")}
+            style={{
+              width: 40,
+              height: 40,
+            }}
+          />
+          <Text style={{ color: "white" }}>Check In</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -128,18 +223,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  textStyle: {
-    flex: 1,
-    fontSize: 26,
-    fontWeight: "bold",
-    color: "white",
-    paddingTop: 30,
-  },
+
   map: {
     width: width,
     height: height,
   },
-  userLocation: {
+  userLocationContainer: {
     position: "absolute",
     width: "90%",
     backgroundColor: "#ffa31a",
@@ -151,10 +240,15 @@ const styles = StyleSheet.create({
     padding: 8,
     borderRadius: 8,
     color: "#ffffff",
+    top: Constants.statusBarHeight,
   },
-  input: {
-    borderColor: "#888",
-    borderWidth: 1,
-    borderRadius: 8,
+  checkIn: {
+    position: "absolute",
+    alignItems: "center",
+    bottom: 100,
   },
 });
+
+// to improve
+// -dynamic location and destination wording on drag end
+// -dynamic floating tootltip for pins on dragend. and on creation

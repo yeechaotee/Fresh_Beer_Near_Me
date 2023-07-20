ca49385687668b23c4aaa13fb47639f7bef9068e
janmichaelortega <janmichaelortega1994@gmail.com>
2023-07-20 10:50:12 +0800

    moving files around

--- navigation/screens/MapsScreen.js
+++ navigation/screens/MapsScreen.js
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
-import darkModeStyle from "./darkMode.json";
+import darkModeStyle from "../darkModeStyle";
 import Constants from "expo-constants";
 import InputAutoComplete from "./InputAutoComplete";
 import { endAsyncEvent } from "react-native/Libraries/Performance/Systrace";

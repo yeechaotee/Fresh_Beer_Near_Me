08d3302a1dac7fac10f1789bcfdeb7d38d8b69e8
TEEYC <yechaotee1995@gmail.com>
2023-08-09 17:10:02 +0800

    Yc navigation discovery (#50)
    * major updates for:
    - Venue management
    - MenuItems management
    - Venue MenuItems integrated with firebase
    - Location registration & Operating Hour on Venue Creation
    - Scrollable menuItems UI (in-progress)
    
    * testing compilation

--- components/VenueDetail/About.js
+++ components/VenueDetail/About.js
@@ -2,6 +2,7 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+// import ActionButton from 'react-native-action-button';
 
 const yelpVenueInfo = {
     name: 'Farmhouse kitchen Thai Cuisine',
@@ -20,7 +21,7 @@ const yelpVenueInfo = {
 
 export default function About(props) {
     //props.route.params is the object contains all these info from route
-    const { name, image, price, reviews, rating, categories, caption, manageable } = props.route.params;
+    const { name, image, price, reviews, rating, categories, caption, manageable, operating_hour, location, venueId } = props.route.params;
 
     const formattedCategories = categories.map((cat) => cat).join(" • ")
 
@@ -39,30 +40,62 @@ export default function About(props) {
                     position: 'absolute',
                     paddingVerticle: 9,
                     borderRadius: 20,
-                    backgroundColor: 'black',
                 }}>
                 {/* <Text style={{ fontSize: 17, color: '#fff' }}>◀Back</Text> */}
                 <FontAwesome5 name={'arrow-alt-circle-left'} color={'#fff'} size={30} />
             </TouchableOpacity>
             {manageable &&
-                <TouchableOpacity
-                    style={{
-                        marginLeft: 20,
-                        top: 30,
-                        right: 5,
-                        zIndex: 1,
-                        position: 'absolute',
-                        paddingVerticle: 9,
-                        borderRadius: 20,
-                        backgroundColor: 'black',
-                    }}>
-                    {/* <Text style={{ fontSize: 17, color: '#fff' }}>◀Back</Text> */}
-                    <FontAwesome5 name={'arrow-alt-circle-left'} color={'#fff'} size={30} />
-                </TouchableOpacity>
+                <>
+                    <TouchableOpacity
+                        style={{
+                            marginLeft: 20,
+                            top: 30,
+                            right: 5,
+                            zIndex: 1,
+                            position: 'absolute',
+                            paddingVerticle: 9,
+                            borderRadius: 20,
+                        }}>
+                        {/* <Text style={{ fontSize: 17, color: '#fff' }}>◀Back</Text> */}
+                        <FontAwesome5 name={'cogs'} color={'orange'} size={30} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.push('EditMenuItemScreen', {
+                        venueId: venueId,
+                    })}
+                        style={{
+                            marginLeft: 20,
+                            top: 70,
+                            right: 8,
+                            zIndex: 1,
+                            position: 'absolute',
+                            paddingVerticle: 9,
+                            borderRadius: 20,
+                        }}>
+                        {/* <Text style={{ fontSize: 17, color: '#fff' }}>◀Back</Text> */}
+                        <FontAwesome5 name={'plus-circle'} color={'orange'} size={30} />
+                    </TouchableOpacity>
+                    {/* <ActionButton
+                        buttonColor='orange'
+                        style={{
+                            marginLeft: 20,
+                            top: 20,
+                            right: 5,
+                            zIndex: 1,
+                            position: 'absolute',
+                            paddingVerticle: 9,
+                            borderRadius: 20,
+                        }}
+                        onPress={() => navigation.push('EditMenuItemScreen', {
+                            venueId: venueId,
+                        })} /> */}
+                </>
             }
+
             <VenueImage image={image} />
+
             <VenueName name={name} />
-            <VenueCaption caption={caption} />
+            <VenueCaption caption={caption} operating_hour={operating_hour} location={location} />
+
             {/* <VenueDescription description={description} /> */}
         </View>
     );
@@ -73,23 +106,42 @@ const VenueImage = (props) => (
 )
 
 const VenueName = (props) => (
-    <Text
-        style={{
-            fontSize: 29,
-            fontWeight: "600",
-            marginTop: 10,
-            marginHorizontal: 15,
-        }}>{[props.name]}</Text>
+    <>
+        <Text
+            style={{
+                fontSize: 29,
+                fontWeight: "600",
+                marginTop: 10,
+                marginHorizontal: 15,
+            }}>{[props.name]}</Text>
+
+    </>
 )
 
 const VenueCaption = (props) => (
-    <Text
-        style={{
-            fontSize: 15.5,
-            fontWeight: "400",
-            marginTop: 10,
-            marginHorizontal: 15,
-        }}>{[props.caption]}</Text>
+    <>
+        <Text
+            style={{
+                fontSize: 15.5,
+                fontWeight: "400",
+                marginTop: 10,
+                marginHorizontal: 15,
+            }}>{[props.caption]}</Text>
+        <Text
+            style={{
+                fontSize: 15,
+                fontWeight: "500",
+                marginTop: 10,
+                marginHorizontal: 15,
+            }}>Operating Hour: {[props.operating_hour]}</Text>
+        <Text
+            style={{
+                fontSize: 15,
+                fontWeight: "500",
+                marginTop: 10,
+                marginHorizontal: 15,
+            }}>Located at: {[props.location]}</Text>
+    </>
 )
 
 // const VenueDescription = (props) => (

1ba9440cc3ab52f245ca0634e347d4142e690a3e
sshinchian <cindysiaw98@gmail.com>
2023-08-18 21:03:08 +0800

    notification added

--- components/TabNotif.js
+++ components/TabNotif.js
@@ -26,7 +26,9 @@ const HeaderButton = (props) => (
 );
 
 const TabNotif = ({ userRole }) => {
-  const [activeTab, setActiveTab] = useState('Promotion');
+  const [activeTab, setActiveTab] = useState("Activity");
+
+
   const [activeData, setActiveData] = useState([]);
 
 
@@ -39,29 +41,23 @@ const TabNotif = ({ userRole }) => {
     }
 
     try {
+
       const notificationsRef = collection(FIRESTORE_DB, 'notifications');
-      const q = tabName === "News Feed" ?
+      const q =
+        /*  tabName === "Activity" ?
+            query(
+              notificationsRef,
+              where('owner_uid', '==', FIREBASE_AUTH.currentUser.uid),
+              where('type', 'in', ['Rating', 'verification']),
+              orderBy('timestamp', 'desc')
+            )
+            :*/
         query(
           notificationsRef,
           where('owner_uid', '==', FIREBASE_AUTH.currentUser.uid),
-          where('type', '==', "userpost"),
+          where('type', '==', tabName),
           orderBy('timestamp', 'desc')
-        )
-        :
-        tabName === "Activity" ?
-          query(
-            notificationsRef,
-            where('owner_uid', '==', FIREBASE_AUTH.currentUser.uid),
-            where('type', 'in', ['rating', 'verification']),
-            orderBy('timestamp', 'desc')
-          )
-          :
-          query(
-            notificationsRef,
-            where('owner_uid', '==', FIREBASE_AUTH.currentUser.uid),
-            where('type', '==', tabName),
-            orderBy('timestamp', 'desc')
-          );
+        );
 
 
       const querySnapshot = await getDocs(q);
@@ -133,18 +129,19 @@ const TabNotif = ({ userRole }) => {
   const tabsBD = [
     /*{ id: '1', name: 'Activity' },
      */
+    { id: '4', name: 'Activity' },
     { id: '2', name: 'Promotion' },
     { id: '3', name: 'Event' },
-    { id: '4', name: 'New Venue' },
+
 
     // Add more tabs as needed
   ];
 
   const tabsBO = [
     { id: '1', name: 'Activity' },
-    { id: '2', name: '         ' },
-    { id: '3', name: '         ' },
-    { id: '3', name: '         ' },
+    { id: '2', name: '                                    ' },
+    //{ id: '3', name: '         ' },
+    //{ id: '3', name: '         ' },
     //{ id: '4', name: 'News Feed' },
     // Add more tabs as needed
   ];
